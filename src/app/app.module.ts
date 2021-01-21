2c5af3530b7a67bb5eafc111c41dfa953896d583
daniel.monroy <daniel.monroym@udea.edu.co>
2021-01-21 11:54:55 -0500

    Home page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { NewarrivalComponent } from './components/newarrival/newarrival.component';
+import { ImagesComponent } from './components/images/images.component';
+import { PopularproductsComponent } from './components/popularproducts/popularproducts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CategoriesComponent,
+    NewarrivalComponent,
+    ImagesComponent,
+    PopularproductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
